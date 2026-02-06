c6b231f84abf782c127e77014e1ef4641f49b7c4
SONAL GUPTA <136976424+SSONALGUPTA@users.noreply.github.com>
2026-02-06 20:56:11 +0530

    Delete src/main directory

--- src/main/java/com/bugtracker/repository/IssueRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.bugtracker.repository;
-
-import com.bugtracker.entity.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<Issue,Long>{}
